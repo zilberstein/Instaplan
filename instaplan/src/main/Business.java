33fa784f955f51a31957788582a054ec28f1d446
Susan Greenberg <susangr@seas.upenn.edu>
2013-04-09 16:35:21 -0400

    java project, I think this is right

---
+++ instaplan/src/main/Business.java
@@ -0,0 +1,17 @@
+package main;
+
+public class Business {
+	
+	public int id=0;
+	public String name;
+	public String address;
+	public String city;
+	public String state;
+	public float lat;
+	public float lon;
+	public int stars;
+	public String photo;
+	
+	
+
+}
