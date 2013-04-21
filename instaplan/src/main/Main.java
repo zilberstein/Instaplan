d52c03c98910c9923c488a0a888aeab6b9f3c84b
Sung Won Hwang <sunghw@gmail.com>
2013-04-21 09:39:40 -0400

    db populating done, commented too

--- instaplan/src/main/Main.java
+++ instaplan/src/main/Main.java
@@ -10,6 +10,7 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
@@ -24,10 +25,11 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
 public class Main {
-
-	static HashMap<String, String> ycatOurCat = new HashMap<String, String>();
-	static ArrayList<Category> our_categories = new ArrayList<Category>();
 	
+	//yelpCategory -> set[OurCategory] map
+	static HashMap<String, HashSet<String>> ycatOurCat = new HashMap<String, HashSet<String>>();
+	//collections for DB
+	static ArrayList<Category> our_categories = new ArrayList<Category>();
 	static ArrayList<Business> businesses = new ArrayList<Business>();
 	static ArrayList<Review> reviews = new ArrayList<Review>();
 	static ArrayList<YelpUser> yusers = new ArrayList<YelpUser>();
@@ -56,12 +58,13 @@ public class Main {
 	 */
 	public static void categorySetUp() throws Exception{
 		ArrayList<String> ourCategories = new ArrayList<String>();
-		String[] categories = {"active", "breakfast", "cats", "college", 
+		String[] categories = {"active", "breakfast", "college", 
 				"culture", "dessert", "dinner", "family", "kids", 
 				"lunch", "nightlife", "old_people","pamper"};
 		Arrays.sort(categories);
 		ourCategories.addAll(Arrays.asList(categories));
 		
+		//populate Category ArrayList
 		for(String c: categories){
 			Category category = new Category(c);
 			our_categories.add(category);
@@ -73,16 +76,23 @@ public class Main {
 			BufferedInputStream bis = new BufferedInputStream(fis);
 			BufferedReader d= new BufferedReader(new InputStreamReader(bis));		
 			String ycat = "";
+			
 			while((ycat = d.readLine()) !=null){
 				if(!ycatOurCat.containsKey(ycat)){
-					ycatOurCat.put(ycat, oc);
-					System.out.println(ycat + " " +oc + "populated into Map");
+					ycatOurCat.put(ycat, new HashSet<String>());
 				}
+				HashSet<String> catVals = ycatOurCat.get(ycat);
+				catVals.add(oc);
+				ycatOurCat.put(ycat, catVals);
 			}
+			
 			fis.close();
 			bis.close();
 			d.close();
 		}
+		//dump ycatOurCat to check
+		Gson gson = new Gson();
+		System.out.println(gson.toJson(ycatOurCat));
 	}
 	
 
@@ -92,9 +102,7 @@ public class Main {
 	 */
 	public static void importData() throws Exception{
 		
-		
 		Gson gson = new Gson();
-		
 		//convertJSON to objects
 		String json = "";
 		File file = new File("yelp_academic_dataset.json");
@@ -102,69 +110,84 @@ public class Main {
 		BufferedInputStream bis = new BufferedInputStream(fis);
 		BufferedReader d= new BufferedReader(new InputStreamReader(bis));		
 		
+		//for test sake:
 		boolean userstarted= false;
 		boolean bizStarted = false;
 		boolean rvStarted = false;
+		System.out.println("num_entries of ycatOurCat:"+ycatOurCat.entrySet().size());
+		
 		while((json = d.readLine())!=null){
-			//
+
 			Map<String,Object> map=new HashMap<String,Object>();
 			map=(Map<String,Object>) gson.fromJson(json, map.getClass());
 			Object type = map.get("type");
 			
 			if(type.equals("user")){
-				if(userstarted == false){
+				
+				if(userstarted == false){ //test
 					userstarted=true;
 					System.out.println("userStart");
 				}
-				JsonUser juser;
-				juser = gson.fromJson(json, JsonUser.class);
+				
+				JsonUser juser = gson.fromJson(json, JsonUser.class);
 				Votes v = juser.votes;
 				juser.name =URLEncoder.encode(juser.name, "UTF-8");
+				//convert to YelpUser and add to yusers array
 				YelpUser yuser = new YelpUser(juser.user_id, juser.review_count, juser.average_stars, v.useful, v.funny, v.cool );
 				yusers.add(yuser);
 			}
-			else if(type.equals("business")){
+			else if(type.equals("business")){//add businessto collection
 				
 				if(bizStarted == false){
 					bizStarted=true;
 					System.out.println("bizStart");
-					System.out.println(json);
 				}
+				
 				JsonBusiness jb= gson.fromJson(json, JsonBusiness.class);
 				jb.name = URLEncoder.encode(jb.name, "UTF-8");
 				jb.full_address = URLEncoder.encode(jb.full_address, "UTF-8");
 				Business b = new Business(jb.business_id, jb.name, jb.full_address, jb.city, jb.state, jb.latitutde, jb.longitude, jb.stars, jb.review_count, 0, jb.photo_url);
 				
 				for(String yc : jb.categories){
-					if(ycatOurCat.containsKey(yc)){
-						String ourc = ycatOurCat.get(yc);
-						b.addCategory(new Category(ourc));
+					//lowercase, replace whitespaces for yelp categories
+					yc = yc.toLowerCase();
+					String yc1 = yc.replaceAll(" ", "_");
+					String yc2 = yc.replaceAll(" ", "");
+					
+					if(ycatOurCat.containsKey(yc1)){
+						
+						HashSet<String> catSet = ycatOurCat.get(yc1);
+						for(String c: catSet){
+							b.addCategory(new Category(c));
+						}
+					}
+					else if(ycatOurCat.containsKey(yc2)){
+						
+						HashSet<String> catSet = ycatOurCat.get(yc2);
+						for(String c: catSet){
+							b.addCategory(new Category(c));
+						}
 					}
 				}
 				businesses.add(b);
 			}
-			else if(type.equals("review")){
+			else if(type.equals("review")){ //add review to collection
 				
 				if(rvStarted == false){
 					rvStarted=true;
 					System.out.println("reviewStart");
 				}
-				JsonReview jr;
-				jr = gson.fromJson(json, JsonReview.class);
-				jr.text = "";
+				JsonReview jr = gson.fromJson(json, JsonReview.class);
+				jr.text = ""; //we don't neeed review texts. 
 				Votes v = jr.votes;
 				Review r = new Review(jr.business_id, jr.user_id, jr.stars, v.useful, v.funny, v.cool );
 				reviews.add(r);
-				
-				//jReviews.add(jreview);
 			}
-			
 		}
 
 		d.close();
 		fis.close();
 		bis.close();
-
 	}
 	
 
@@ -173,13 +196,10 @@ public class Main {
 		PreparedStatement preparedStatement = null;
 		try {
 			// This will load the MySQL driver, each DB has its own driver
-		      
 			Class.forName("com.mysql.jdbc.Driver");
 			Connection con = null;
 			if (args.length <2 ) {
-				con = DriverManager.getConnection(
-					
-						"jdbc:mysql://SQL09.FREEMYSQL.NET/instaplan?"+
+				con = DriverManager.getConnection("jdbc:mysql://SQL09.FREEMYSQL.NET/instaplan?"+
 								"user=instaplan&password=cis330");
 			} else {
 				con = DriverManager.getConnection(
@@ -196,7 +216,7 @@ public class Main {
 		return null;
 	}
 	
-	
+	//NOT USING IT
 	private static String safeDropTable(String table) {
 		String query = "Begin Execute Immediate 'Drop Table "
 				+ table
@@ -205,9 +225,10 @@ public class Main {
 	}
 	
 	
-	
+	//commented out yelpUser, review as well as writes. feel free to take it back.
 	public static void DDLs(Statement st) throws Exception {
 		try {
+			//safe drop
 			st.execute("DROP TABLE IF EXISTS belongs");
 			st.execute("DROP TABLE IF EXISTS review");
 			st.execute("DROP TABLE IF EXISTS business");
@@ -283,7 +304,7 @@ public class Main {
 		String[] categories = {"active", "breakfast", "cats", "college", 
 				"culture", "dessert", "dinner", "family", "footer", "kids", 
 				"lunch", "nightlife", "old_people","pamper"};
-		/*for (String c : categories) {
+		for (String c : categories) {
 			String t = "INSERT INTO category VALUES ('" + c + "')";
 			try {
 				st.executeUpdate(t);
@@ -291,11 +312,15 @@ public class Main {
 				e.printStackTrace();
 				break;
 			}
-		}*/
+		}
 		int i = 0;
-		/*System.out.println("LOAD Businesses");
+		System.out.println("LOAD Businesses");
 		for (Business b : businesses) {
 			i++;
+			if(i>=1000 && i%1000==0){
+				System.out.println(i+"th Business reached");
+				
+			}
 			if(i==10000)
 				System.out.println("10,000th Business reached");
 			
@@ -310,11 +335,10 @@ public class Main {
 				System.out.println(i);
 				Gson gson = new Gson();
 				System.out.println(gson.toJson(b));
-				e.printStackTrace();
+				//e.printStackTrace();
 				
-				break;
 			}
-		}*/
+		}
 		i=0;
 		/*System.out.println("LOAD YelpUsers");
 		for (YelpUser y : users) {
@@ -334,7 +358,9 @@ public class Main {
 				Gson gson = new Gson();
 				System.out.println(gson.toJson(y));
 				e.printStackTrace();
+				
 				break;
+				//or do nothing and continue
 			}
 		}
 		*/
@@ -344,8 +370,6 @@ public class Main {
 			i++;
 			if(i==10000)
 				System.out.println("10,000th reached");
-			if(i==100000)
-				System.out.println("100,000th reached");
 			if(i==300000)
 				System.out.println("300,000th reached");
 			
@@ -358,26 +382,38 @@ public class Main {
 				e.printStackTrace();
 				
 				break;
+				//or do nothing and continue
 			}
 		}*/
 		
 		System.out.println("LOAD business_belongsTo_categories");
 		int in=0;
+		
 		for (Business b : businesses) {
 			
 			in++;
-			System.out.println("hit1");
+			if(in==5000){
+				System.out.println(in+"th reached");
+			}
+			if(in == 10000){
+				System.out.println(in+"th reached");
+					
+			}
+			//System.out.println("hit1");
 			for (Category c : b.categories) {
 				try {
-					if(in == 1){System.out.println("first line");}
+					if(in == 1){System.out.print("first line");}
+					
 					String t = "INSERT INTO belongs VALUES ('" + b.id +
 							"', '" + c.name + "')";
 					st.executeUpdate(t);
 				} catch (Exception e) {
-					e.printStackTrace();
+					//e.printStackTrace();
+					//System.out.println("bid:"+ b.id  +" not found; " + "cname:" + c.name +" not found");
 				}
 				
 			}
+			
 		}
 		
 	}
