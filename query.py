d52c3e3bf945827a5c8320505abda26e76caf208
Susan Greenberg <susangr@seas.upenn.edu>
2013-04-21 14:05:26 -0400

    general query works

--- query.py
+++ query.py
@@ -13,33 +13,33 @@ lon = float(sys.argv[7])
 l = days*options
 
 cat = ""
-for k in keywords:
+for k in keywords.split(", "):
        	cat += k + ', '
 cat = cat[:-2]
 a = []
 
-for e in event:
-	e = e.lower()
-	if e=='breakfast' or e=='lunch' or  e=='dinner' or  e=='nightlife' or  e=='overnight':
-		sql = ('select b.name, address, city, state, lat, lon, avg_stars, photo '
-		       'from business as b, belongs as be '
-		       'where id = b_id AND be.name in ('+str(cat)+') '
-		       'AND  (select SQRT(POWER(lat-'+str(lat)+',2) - POWER(lon-'+str(lon)+',2)) '
-		       'from businesses b1 where b.id = b1.id) <= '+str(distance) + ' '
-		       'AND '+ str(e) +'  in (select be1.name '
-		       'from belongs as be1 '
-		       'where be.b_id = be1.b_id) '
-		       'order_by metric '
-		       'limit 0, ' + str(l) + ' ')
-		a.append(sql)
-	else: 
-		sql = ('select b.name, address, city, state, lat, lon, avg_stars, photo '
-		       'from business as b, belongs '
-		       'where id = b_id AND belongs.name in ('+str(cat)+') '
-		       'AND  (select SQRT(POWER(lat-'+str(lat)+',2) - POWER(lon-'+str(lon)+',2)) '
-		       'from businesses b1 where b.id = b1.id) <= '+str(distance) + ' '
-		       'order_by metric '
-		       'limit 0, ' + str(l) + ' ')
->>>>>>> 50f0711d967cdeaaed35d3042ed11d9f349e5f87
+
+for e in event.split(", "):
+#	e = e.lower()
+#	if e=='breakfast' or e=='lunch' or  e=='dinner' or  e=='nightlife' or  e=='overnight':
+#		sql = ('select b.name, address, city, state, lat, lon, avg_stars, photo '
+#		       'from business as b, belongs as be '
+#		       'where id = b_id AND be.name in ('+str(cat)+') '
+#		       'AND  (select SQRT(POWER(lat-'+str(lat)+',2) - POWER(lon-'+str(lon)+',2)) '
+#		       'from businesses b1 where b.id = b1.id) <= '+str(distance) + ' '
+#		       'AND '+ str(e) +'  in (select be1.name '
+#		       'from belongs as be1 '
+#		       'where be.b_id = be1.b_id) '
+#		       'order_by metric '
+#		       'limit 0, ' + str(l) + ' ')
+#		a.append(sql)
+#	else: 
+		sql = ('select b.name, address, city, state, latitude, longitude, stars, photoUrl '
+		       'from business as b, belongs as be'
+		       'where id = businessId AND belongs.name in ('+str(cat)+') '
+		       'AND  (select SQRT(POWER(latitude-'+str(lat)+',2) + POWER(longitude-'+str(lon)+',2)) '
+		       'from businesses as b1 where b.id = b1.id) <= '+str(distance) + ' '
+		       'order by metric '
+		       'limit ' + str(l) + ' ')
 		a.append(sql)
 print "~".join(a)
