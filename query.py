6021f000842908df205f0e678e220d4d8fd6a135
Susan Greenberg <susangr@seas.upenn.edu>
2013-04-21 13:55:40 -0400

    stupid

--- query.py
+++ query.py
@@ -19,26 +19,26 @@ cat = cat[:-2]
 a = []
 
 for e in event:
-	if t=='breakfast' | t=='lunch' | t=='dinner' | t=='nightlife' | t=='overnight':
-		sql = ('select b.name, address, city, state, lat, lon, avg_stars, photo '
+#	if t=='breakfast' | t=='lunch' | t=='dinner' | t=='nightlife' | t=='overnight':
+#		sql = ('select b.name, address, city, state, lat, lon, avg_stars, photo '
+#		       'from business as b, belongs as be '
+#		       'where id = b_id AND be.name in ('+cat+') '
+#		       'AND  (select SQRT(POWER(lat-'+lat+',2) - POWER(lon-'+lon+',2)) '
+#		       'from businesses b1 where b.id = b1.id) <= '+distance + ' '
+#		       'AND '+ e +'  in (select be1.name '
+#		       'from belongs as be1 '
+#		       'where be.b_id = be1.b_id) '
+#		       'order_by metric '
+#		       'limit 0, ' + l + ' ')
+#		a.append(sql)
+#	else: 
+		sql = ('select b.name, address, city, state, latitude, longitude, stars, photoUrl '
 		       'from business as b, belongs as be '
-		       'where id = b_id AND be.name in ('+cat+') '
-		       'AND  (select SQRT(POWER(lat-'+lat+',2) - POWER(lon-'+lon+',2)) '
+		       'where b.id = be.businessId AND belongs.name in ('+cat+') '
+		       'AND  (select SQRT(POWER(latitude-'+lat+',2) + POWER(longitude-'+lon+',2)) '
 		       'from businesses b1 where b.id = b1.id) <= '+distance + ' '
-		       'AND '+ e +'  in (select be1.name '
-		       'from belongs as be1 '
-		       'where be.b_id = be1.b_id) '
-		       'order_by metric '
-		       'limit 0, ' + l + ' ')
-		a.append(sql)
-	else: 
-		sql = ('select b.name, address, city, state, lat, lon, avg_stars, photo '
-		       'from business as b, belongs '
-		       'where id = b_id AND belongs.name in ('+cat+') '
-		       'AND  (select SQRT(POWER(lat-'+lat+',2) - POWER(lon-'+lon+',2)) '
-		       'from businesses b1 where b.id = b1.id) <= '+distance + ' '
-		       'order_by metric '
-		       'limit 0, ' + l + ' ')
+		       'order by metric DESC '
+		       'limit '+ l + ' ')
 		a.append(sql)
 		print "~".join(a)
 		
